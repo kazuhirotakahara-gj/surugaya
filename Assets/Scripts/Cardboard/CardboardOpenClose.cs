59271f3ac34ad3f191f4baef74b0ca5ad091dc13
kazuhirotakahara-gj <kazuhiro.takahara.gj201903@gmail.com>
2019-03-09 14:14:57 +0900

    とりあえずあげる

---
+++ Assets/Scripts/Cardboard/CardboardOpenClose.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardboardOpenClose : MonoBehaviour
+{
+    private UnityEngine.SpriteRenderer SpriteRenderer = null;
+    public UnityEngine.Sprite OpenSprite = null;
+    public UnityEngine.Sprite CloseSprite = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer = GetComponent<UnityEngine.SpriteRenderer>();
+        if (SpriteRenderer == null) throw new System.Exception("No SpriteRenderer component.");
+
+        SpriteRenderer.sprite = OpenSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseClick()
+    {
+        SpriteRenderer.sprite = CloseSprite;
+    }
+}
