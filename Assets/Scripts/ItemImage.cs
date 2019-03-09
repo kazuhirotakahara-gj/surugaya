fb4ab5ce77ef78b8562baee2521af5d2a16a72ff
sechino <tempura.eggplant@gmail.com>
2019-03-09 16:24:06 +0900

    アイテムのドラッグアンドドロップ実装途中 マウスの座標が2次元座標になっているため、3d変換処理が必要

--- Assets/Scripts/ItemImage.cs
+++ Assets/Scripts/ItemImage.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class ItemImage : MonoBehaviour
 {
+    public enum ItemImageState
+    {
+        Invalid,
+
+        PickMouse,
+
+        ReleaseMouse,
+
+        AtBox,
+
+        AtBelt,
+    }
+
+    public ItemImageState State;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        State = ItemImageState.AtBelt;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    /// <summary>
+    /// MouseでPickしたい
+    /// </summary>
+    public bool TryMousePick()
+    {
+        if (State != ItemImageState.AtBelt)
+            return false;
+
+        State = ItemImageState.PickMouse;
+        return true;
+    }
+
+    /// <summary>
+    /// MouseのPickをキャンセル
+    /// </summary>
+    public bool TryMouseRelease()
+    {
+        if (State != ItemImageState.PickMouse)
+            return false;
+
+        State = ItemImageState.AtBelt;
+
+        // 補完いる？
+        transform.position = Vector3.zero;
+        return true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void TryOutBox()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void TryInBox()
+    {
+
     }
 }
--- Assets/Scripts/MouseSystem.cs
+++ Assets/Scripts/MouseSystem.cs
@@ -19,11 +19,26 @@ public class MouseSystem : MonoBehaviour
 
     public bool Trace = true;
 
+    //Rayの長さ
+    public float _MaxDistance = 500;
+
     Vector3 _MouseDownPosition = Vector3.zero;
 
     Vector3 _LastPosition = Vector3.zero;
 
+    /// <summary>
+    /// マウスダウンした直後において
+    /// pickしたアイテムと、マウスの位置の差
+    /// </summary>
+    Vector3 _PickItemMouseDiff = Vector3.zero;
+
+    public GameObject PickItemImageObject = null;
+
     public MouseState State = MouseState.Invalid;
+
+    public LayerMask _Mask;
+
+    public ItemImage PickItemImageComponent;
 
     // Start is called before the first frame update
     void Start()
@@ -86,6 +101,7 @@ public class MouseSystem : MonoBehaviour
         if (_LastPosition == pos)
             return;
 
+        SyncPickItemImageObjectPos(pos);
         if (Trace)
             Debug.Log("MouseDrag:" + pos.ToString());
 
@@ -96,6 +112,9 @@ public class MouseSystem : MonoBehaviour
     {
         _MouseDownPosition = Vector3.zero;
         State = MouseState.MouseMove;
+
+        // とりあえず放す
+        SetPickItemImageObject(null);
 
         if (Trace)
             Debug.Log("MouseUp:" + pos.ToString());
@@ -121,6 +140,55 @@ public class MouseSystem : MonoBehaviour
 
     void RayCastItem(Vector3 pos)
     {
+        //メインカメラ上のマウスカーソルのある位置からRayを飛ばす
+        var ray = Camera.main.ScreenPointToRay(pos);
+        var hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, _MaxDistance, _Mask);
 
+        //なにかと衝突した時だけそのオブジェクトの名前をログに出す
+        if (hit.collider)
+        {
+            var go  = hit.collider.gameObject;
+            SetPickItemImageObject(go);
+        }
+    }
+
+    void SetPickItemImageObject(GameObject obj)
+    {
+        if (PickItemImageObject == null && obj == null)
+            return;
+
+        if(PickItemImageComponent != null)
+            PickItemImageComponent.TryMouseRelease();
+
+        var img = obj?.GetComponent<ItemImage>();
+        var result = false;
+
+        if (img != null)
+            result = img.TryMousePick();
+
+        if(result)
+        {
+            PickItemImageObject     = obj;
+            PickItemImageComponent  = img;
+
+            _PickItemMouseDiff      = obj.transform.position - _MouseDownPosition;
+        }
+        else
+        {
+            PickItemImageObject     = null;
+            PickItemImageComponent  = null;
+
+            _PickItemMouseDiff      = Vector3.zero;
+        }
+
+    }
+
+    void SyncPickItemImageObjectPos(Vector3 pos)
+    {
+        if (PickItemImageObject == null)
+            return;
+
+        PickItemImageObject.transform.position = pos;
+        //PickItemImageObject.transform.position = pos + _PickItemMouseDiff;
     }
 }
