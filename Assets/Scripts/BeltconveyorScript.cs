3d44375d1284b8bd48ffa3cfe7e44acd485ca0fa
yuzukey <yuzukey@users.noreply.github.com>
2019-03-09 17:44:36 +0900

    ベルトコンベアのカバー追加 スクリプトの変更し、親要素で編集できるように

--- Assets/Scripts/BeltconveyorScript.cs
+++ Assets/Scripts/BeltconveyorScript.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class BeltconveyorScript : MonoBehaviour
 {
-    public float startposition = 0;
-    public float endposition = 0;
-    public float speed = 0;
-
     Conveyor _Conveyor;
     // Start is called before the first frame update
     void Start()
     {
         var parent = gameObject.transform.parent;
         _Conveyor = parent?.GetComponent<Conveyor>();
-        //startposition = 10;//Conveyor1.transform.position.x + Conveyor1.GetComponent<RectTransform>().sizeDelta.x;
-        //endposition = -10;
+        
     }
 
     // Update is called once per frame
@@ -26,12 +21,12 @@ public class BeltconveyorScript : MonoBehaviour
 
         Vector3 nowposition = this.transform.position;
 
-        nowposition.x += speed * Time.deltaTime;
+        nowposition.x += _Conveyor.speed * Time.deltaTime;
         this.transform.position = nowposition;
 
-        if (nowposition.x < endposition)
+        if (nowposition.x < _Conveyor.endposition)
         {
-            nowposition.x = startposition;
+            nowposition.x = _Conveyor.startposition;
             this.transform.position = nowposition;
         }
     }
--- Assets/Scripts/Conveyor.cs
+++ Assets/Scripts/Conveyor.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Conveyor : MonoBehaviour
 {
+    public float startposition = 0;
+    public float endposition = 0;
+    public float speed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
