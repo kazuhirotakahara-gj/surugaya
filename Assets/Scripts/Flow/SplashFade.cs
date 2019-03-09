a4d69c4f8f33db3212f620765904f0463ccaead4
kazuhirotakahara-gj <kazuhiro.takahara.gj201903@gmail.com>
2019-03-09 15:23:10 +0900

    スプラッシュのキャンセル対応

--- Assets/Scripts/Flow/SplashFade.cs
+++ Assets/Scripts/Flow/SplashFade.cs
@@ -54,20 +54,32 @@ public class SplashFade : MonoBehaviour
             case FadeStatus.Wait:
                 WaitTimer -= Time.deltaTime;
                 if (WaitTimer <= 0.0f) Status = FadeStatus.FadeIn;
+                if (Input.anyKeyDown)
+                {
+                    TargetAlpha = 1.0f;
+                    Status = FadeStatus.Keep;
+                }
                 break;
 
             case FadeStatus.FadeIn:
                 TargetAlpha += Time.deltaTime * FadeSpeed;
                 KeepTimer = KeepTime;
+                if (Input.anyKeyDown)
+                {
+                    TargetAlpha = 1.0f;
+                    Status = FadeStatus.Keep;
+                }
                 break;
 
             case FadeStatus.Keep:
                 KeepTimer -= Time.deltaTime;
                 if (KeepTimer <= 0.0f) Status = FadeStatus.FadeOut;
+                if (Input.anyKeyDown) Status = FadeStatus.End;
                 break;
                
             case FadeStatus.FadeOut:
                 TargetAlpha -= Time.deltaTime * FadeSpeed;
+                if (Input.anyKeyDown) Status = FadeStatus.End;
                 break;
 
             case FadeStatus.End:
---
+++ Assets/Scripts/Flow/TitleMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickCampaignMode()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Splash0");
+    }
+
+    public void OnClickExtraContents()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Splash1");
+    }
+
+    public void OnClickOptions()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Splash2");
+    }
+}
