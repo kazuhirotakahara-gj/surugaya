8113f6c7d53fb4fb0a866ca4c8a849ad09a3340e
yuzukey <yuzukey@users.noreply.github.com>
2019-03-10 14:45:45 +0900

    GJを表示するプレハブを追加 一旦、表示時間は2秒

---
+++ Assets/Scripts/DisplayGood.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayGood : MonoBehaviour
+{
+    public float CountTime = 0f;
+    float timecount = 0f;
+    public GameObject GJ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timecount = CountTime;
+        GJ.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timecount -= Time.deltaTime;
+        if (timecount < 0)
+        {
+            GJ.SetActive(false);
+        }
+    }
+}
